79411a16b817e8876bca18bc61e6e0f6bee5cf2e
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-01-05 01:24:00 +0530

    Finding multiple missing array element in sorted array

---
+++ 4. Array ADT/finding_multiple_missing_array_element_sorted_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void find_missing_element(int array[],int left,int right,int number)
+{
+  int i;
+  int diff=left-0;
+  for (i=0;i<number;i++)
+  {
+    if (array[i]-i!=diff)
+    {
+      while (diff<array[i]-i)
+      {
+        printf("Missing element is : %d",i+diff);
+        diff++;
+      }
+    }
+  }
+}
+
+int main()
+{
+  int a[11]={6,7,8,9,11,12,15,16,17,18,19};
+
+  find_missing_element(a,6,19,11);
+
+  return 0;
+}
