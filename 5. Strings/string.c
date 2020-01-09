6294e6ff40ddc81833a7581e616f9901550518b7
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-01-09 08:18:34 +0530

    String

---
+++ 5. Strings/string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+  char x[5];
+  char xx[5]={'a','b','c','d','e'};
+  char xxx[]={'a','b','c','d','e'};
+  char xxxx[5]={65,66,67,68,69};
+  char xxxxx[5]={'a','b'};
+  //Declaring and drfining character array(string).
+
+
+  return 0;
+}
