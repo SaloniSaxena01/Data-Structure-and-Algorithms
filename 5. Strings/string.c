a9341747bd84b6029d1731ccb562c6a4f9e6b664
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-01-09 08:26:39 +0530

    Update README.md

--- 5. Strings/string.c
+++ 5. Strings/string.c
@@ -8,7 +8,16 @@ int main()
   char xxx[]={'a','b','c','d','e'};
   char xxxx[5]={65,66,67,68,69};
   char xxxxx[5]={'a','b'};
-  //Declaring and drfining character array(string).
+  //Declaring and defining character array.
+
+  char name[10]={'K','s','h','i','t','i','z','\0'};
+  char name_a[]={'K','s','h','i','t','i','z','\0'};
+  char name_b[]="Kshitiz";
+  //Created in stack.
+  char *name_c ="Kshitiz";
+  //Created in heap.
+  //Declaring and defining string.
+  // '\0' is the end of string character also known as String Delimeter, NULL Character or String Terminator.
 
 
   return 0;
