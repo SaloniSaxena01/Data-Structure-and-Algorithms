2fdc1ac435d94c11ec662c361a120ee422f8685c
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-01-19 12:36:12 +0530

    maximum in linked list

---
+++ 8. Linked List/maximum_element_in_linked_list.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+struct Node
+{
+  int data;
+  struct Node *next;
+}*first=NULL;
+
+void create(int A[],int n)
+{
+  int i;
+  struct Node *t,*last;
+
+  first=(struct Node *)malloc(sizeof(struct Node));
+  first->data=A[0];
+  first->next=NULL;
+  last=first;
+  for(i=1;i<n;i++)
+  {
+    t=(struct Node*)malloc(sizeof(struct Node));
+    t->data=A[i];
+    t->next=NULL;
+    last->next=t;
+    last=t;
+  }
+}
+
+int Max(struct Node *p)
+{
+  int max=INT_MIN;
+
+  while(p)
+  {
+    if(p->data>max)
+    max=p->data;
+    p=p->next;
+  }
+  return max;
+}
+
+int Recursive_max(struct Node *p)
+{
+  int x=0;
+
+  if(p==0)
+  {
+    return INT_MIN;
+  }
+  x=Recursive_max(p->next);
+  if(x>p->data)
+  {
+    return x;
+  }
+  else
+  {
+    return p->data;
+  }
+}
+
+int main()
+{
+  int A[]={3,5,7,10,25,8,32,2};
+
+  create(A,8);
+
+  printf("Max : %d\n",Max(first));
+
+  printf("\n\n\n\n");
+
+  printf("Max : %d\n",Recursive_max(first));
+
+  return 0;
+}
